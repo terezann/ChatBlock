96ba68bc1ea62f7d37e381600044c415b0e92f90
Pantelis Emmanouil <pantelios2001@gmail.com>
2024-03-26 20:34:58 +0200

    validate_chain added

--- ChatBlock/models/node.py
+++ ChatBlock/models/node.py
@@ -145,8 +145,9 @@ class Node:
     
 
     def validate_chain(self, chain):
-        pass
-        #check for the longer chain across all nodes
+        self.blockchain.append(chain[0])  # add genesis block to blockchain
+        for block in chain[1:]:
+            self.validate_block(block)
         
 my_node1 = Node()
 my_node2 = Node()
