9b2f9dc6d9826b87dcc4584251e63485f8a394b2
terezann <4tereza@gmail.com>
2024-04-12 20:31:45 +0300

    mi dikaiosini gia node1

--- ChatBlock/models/node.py
+++ ChatBlock/models/node.py
@@ -467,7 +467,8 @@ class Node:
 
 def process_transactions(node):
     time.sleep(0.5)
-    node.stake(10)
+    if node.id == 1: node.stake(100)
+    else: node.stake(10)
     # if node.id == 1:
     #     node.stake(100)
     # else:
